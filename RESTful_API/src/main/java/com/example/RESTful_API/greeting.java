03aeaa9014ca97a6e2be60322926663506de184a
SakshamAryal <sakshamaryal.uni@gmail.com>
2025-03-08 21:50:54 -0500

    api

---
+++ RESTful_API/src/main/java/com/example/RESTful_API/greeting.java
@@ -0,0 +1,6 @@
+package com.example.RESTful_API;
+
+public record Greeting(long id, String Content) {
+	id : 1,
+	Content : "Hello World"
+}
---
+++ RESTful_API/src/main/java/com/example/RESTful_API/greeting_Controller.java
@@ -0,0 +1,16 @@
+package com.example.RESTful_API;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class greeting_Controller {
+    public static final String template = "Hello, %s!";
+    private final Atomiclong counter = new Atomiclong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementandGet(), String.Format(template, name));
+    }
+}
